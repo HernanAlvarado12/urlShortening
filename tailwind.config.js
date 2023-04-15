55e55d1e6176a654366fbc4beea0618d5be837bd
halvaradop_ <hernanvid123@gmail.com>
2023-04-15 11:14:12 -0500

    header for device tables and desktops

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,9 +7,6 @@ export default {
   },
   theme: {
     extend: {
-      screens: {
-        md: '900px'
-      },
       spacing: {
         '0.2': '0.2rem',
         '0.5': '0.5rem',
@@ -17,6 +14,7 @@ export default {
         '1.5': '1.5rem',
         '2': '2rem',
         '3': '3rem',
+        '3.5': '3.5rem',
         '4': '4rem',
         '5': '5rem',
         '6': '6rem',
